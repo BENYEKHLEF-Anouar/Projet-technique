08b69e9b44fc2cdfc2301100aebe1302b0fa0872
BENYEKHLEF-Anouar <benyekhlef.anouar.solicode@gmail.com>
2026-01-06 14:42:56 +0000

    feat: create notes index view with search and filter functionality.

--- notepad/resources/views/notes/index.blade.php
+++ notepad/resources/views/notes/index.blade.php
@@ -19,7 +19,7 @@
             </div>
 
             <!-- Category Dropdown -->
-            <div class="sm:w-64">
+            <div class="sm:w-68">
                 <select name="category" id="category-select"
                         class="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-indigo-500 focus:ring-indigo-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600 shadow-sm">
                     <option value="">All Categories</option>
