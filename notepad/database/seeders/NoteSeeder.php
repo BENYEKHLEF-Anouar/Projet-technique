3ce743b159232e01205b907223ece196540cde0a
BENYEKHLEF-Anouar <benyekhlef.anouar.solicode@gmail.com>
2026-01-12 14:32:42 +0000

    add seed data for notes

--- notepad/database/seeders/NoteSeeder.php
+++ notepad/database/seeders/NoteSeeder.php
@@ -31,7 +31,8 @@ class NoteSeeder extends Seeder
                 // Attach categories
                 $categories = explode('|', $data['categories']); // Splits the categories string by | : 'Work|Project X'
                 $categoryIds = Category::whereIn('name', $categories)->pluck('id'); // Gets the IDs of categories that match the names in the array.
-                $note->categories()->attach($categoryIds); //Uses a many-to-many relationship to attach categories to the note.
+                // $note->categories()->attach($categoryIds); //Uses a many-to-many relationship to attach categories to the note.
+                $note->categories()->sync($categoryIds); //Uses a many-to-many relationship to sync categories to the note (preventing duplicates).
             }
             fclose($handle);
         }
