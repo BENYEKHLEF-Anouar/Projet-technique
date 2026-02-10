465356e0f3ab0cbf657d4e72de8099b9333b86c9
BENYEKHLEF-Anouar <benyekhlef.anouar.solicode@gmail.com>
2026-02-10 12:00:12 +0000

    adjust Alpine setup

--- notepad/resources/views/public/index.blade.php
+++ notepad/resources/views/public/index.blade.php
@@ -91,11 +91,7 @@
                     </div>
                 </div>
 
-                <button type="button" x-show="search || categoryId" @click="clearFilters()"
-                    class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
-                    <i data-lucide="rotate-ccw" class="shrink-0 size-4"></i>
-                    {{ __('note.views.clear') }}
-                </button>
+
             </form>
         </div>
 
